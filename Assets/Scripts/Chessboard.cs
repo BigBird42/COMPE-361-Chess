9d990d2af309eb54dc006c379fb219a8eac39dbe
SkyJohnson <45185895+SkyJohnson@users.noreply.github.com>
2018-11-29 19:01:27 -0800

    Add files via upload
    Created scripts for main game pieces

---
+++ Assets/Scripts/Chessboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessboard : MonoBehaviour {
+
+    public GameObject pawnPrefab, kingPrefab, queenPrefab, bishopPrefab, knightPrefab, rookPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
